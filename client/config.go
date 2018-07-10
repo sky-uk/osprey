8d7770a55b5eac7a9f253402d3f3bf13aa0bffff
Isaac Martinez Aceves <isaac.martinez.aceves@gmail.com>
2018-07-10 09:40:42 +0100

    Fix lint error and small change to CA handling (#15)
    to simplify the condition expressions.

--- client/config.go
+++ client/config.go
@@ -82,28 +82,30 @@ func LoadConfig(path string) (*Config, error) {
 		return nil, fmt.Errorf("invalid config %s: %v", path, err)
 	}
 
-	// Set CAData from PEM-encoded cert file if no CAData
-	if config.CertificateAuthority != "" && config.CertificateAuthorityData == "" {
-		certData, err := web.LoadTLSCert(config.CertificateAuthority)
-		if err != nil {
-			return nil, fmt.Errorf("failed to load global CA certificate: %v", err)
+	if config.CertificateAuthorityData == "" {
+		if config.CertificateAuthority != "" {
+			certData, err := web.LoadTLSCert(config.CertificateAuthority)
+			if err != nil {
+				return nil, fmt.Errorf("failed to load global CA certificate: %v", err)
+			}
+			config.CertificateAuthorityData = certData
 		}
-		config.CertificateAuthorityData = certData
-	} else if config.CertificateAuthorityData != "" {
-    // CA is ignored if CAData is present
+	} else {
+		// CA is overridden if CAData is present
 		config.CertificateAuthority = ""
 	}
 
 	for name, target := range config.Targets {
-		// Set CAData from PEM-encoded cert file if no CAData
-		if target.CertificateAuthority != "" && target.CertificateAuthorityData == "" {
-			certData, err := web.LoadTLSCert(target.CertificateAuthority)
-			if err != nil {
-				return nil, fmt.Errorf("failed to load CA certificate for target %s: %v", name, err)
+		if target.CertificateAuthorityData == "" {
+			if target.CertificateAuthority != "" {
+				certData, err := web.LoadTLSCert(target.CertificateAuthority)
+				if err != nil {
+					return nil, fmt.Errorf("failed to load CA certificate for target %s: %v", name, err)
+				}
+				target.CertificateAuthorityData = certData
 			}
-			target.CertificateAuthorityData = certData
-		} else if target.CertificateAuthorityData != "" {
-      // CA is ignored if CAData is present
+		} else {
+			// CA is overridden if CAData is present
 			target.CertificateAuthority = ""
 		}
 	}
